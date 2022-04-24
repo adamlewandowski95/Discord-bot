a68c75795cdb787cc9e1fbd8e73bcc7147753d23
Mersyliusz <Mersyliusz@gmail.com>
2022-04-24 17:39:23 +0200

    Add RankPngGenerator

---
+++ src/main/java/com/adamlewandowski/Discord_Bot/service/RankPngGenerator.java
@@ -0,0 +1,68 @@
+package com.adamlewandowski.Discord_Bot.service;
+
+import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+@Service
+public class RankPngGenerator {
+    private String path = "src/main/resources";
+    private String defaultFile = "/discord_user_rank.png";
+    private String editedFile = "/discord_user_rank_edited.png";
+
+
+    public File loadImageAndAddText(String name, String avatarUrl, Long rank, Integer points) throws IOException {
+        File f = new File(path);
+        String absolutePath = f.getAbsolutePath();
+
+        File file = new File(absolutePath + defaultFile);
+        File fileEdited = new File(absolutePath + editedFile);
+        BufferedImage avatarBufferedImage = ImageIO.read(new URL(avatarUrl));
+        BufferedImage avatarRounded = makeRoundedAvatar(avatarBufferedImage);
+        Image avatar = avatarRounded.getScaledInstance(110, 110, 0);
+        Font fontDetail = new Font("Arial", Font.CENTER_BASELINE, 12);
+        Font fontHead = new Font("Arial", Font.CENTER_BASELINE, 20);
+
+        BufferedImage bufferedImage = ImageIO.read(file);
+        Graphics g = bufferedImage.getGraphics();
+        g.setFont(fontHead);
+        g.setColor(Color.BLACK);
+        g.drawString(String.format("%s", name), 160, 30);
+        g.drawImage(avatar, 25, 25, null);
+
+        g.setFont(fontDetail);
+        g.setColor(Color.GRAY);
+        g.drawString(String.format("Rank: #%s", rank), 160, 60);
+        g.drawString(String.format("Total: #%s", points), 160, 90);
+        g.dispose();
+        ImageIO.write(bufferedImage, "png", fileEdited);
+
+        return fileEdited;
+    }
+
+    public static BufferedImage makeRoundedAvatar(BufferedImage image) {
+        int w = image.getWidth();
+        int h = image.getHeight();
+        int cornerRadius = 500;
+        BufferedImage output = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D g2 = output.createGraphics();
+
+        g2.setComposite(AlphaComposite.Src);
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(Color.WHITE);
+        g2.fill(new RoundRectangle2D.Float(0, 0, w, h, cornerRadius, cornerRadius));
+
+        g2.setComposite(AlphaComposite.SrcAtop);
+        g2.drawImage(image, 0, 0, null);
+        g2.dispose();
+
+        return output;
+    }
+}
